6f9e19009c3c62a55749cf46c1d619b268445288
Kristian <kris.dziamka@gmail.com>
2022-08-19 22:21:28 +0300

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import axios from "axios";
+import Notiflix from 'notiflix';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const form = document.querySelector('.search-form');
+const submitBtn = document.querySelector('.js-submit-btn');
+const loadMoreBtn = document.querySelector('.load-more');
+const gallery = document.querySelector('.gallery');
